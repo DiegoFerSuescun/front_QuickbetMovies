dd67b72c2eed5c44e04ef0d78052a8b5e51c3d07
DiegoFerSuescun <dfsuesc@gmail.com>
2024-09-05 11:31:53 -0500

    fix[favicon]: new favicon ico import from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import favicon from "../../public/favicon.ico"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,9 @@ export default function RootLayout({
 }>): JSX.Element {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href={favicon.src}/>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
